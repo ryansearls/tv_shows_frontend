4505cf2305a0fb6e02331ed02f7f844bb0911b74
Ryan Searls <Ryan.searls@gmail.com>
2021-10-05 11:09:27 -0600

    create shows index page

---
+++ src/views/ShowsIndex.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="shows-index">
+    <h1>All Shows</h1>
+    <div v-for="show in shows" v-bind:key="show.id">
+      <h2>{{ show.title }}</h2>
+      <img v-bind:src="show.url" v-bind:alt="show.title" />
+      <p>Title: {{ show.title }}</p>
+      <p>Year: {{ show.year }}</p>
+      <p>Description: {{ show.description }}</p>
+      <p>Creator: {{ show.creator }}</p>
+      <p>Seasons: {{ show.seasons }}</p>
+      <p>Network: {{ show.network }}</p>
+      <p>Favorite: {{ show.favorite }}</p>
+      <p>Image: {{ show.image }}</p>
+      <p>User_id: {{ show.user_id }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      shows: [],
+    };
+  },
+  created: function () {
+    this.indexShows();
+  },
+  methods: {
+    indexShows: function () {
+      axios.get("/showsindex").then((response) => {
+        console.log("shows index", response);
+        this.photos = response.data;
+      });
+    },
+  },
+};
+</script>
